279aab22b2ac60e5b8e45e891cbc7df6941dcbf8
David J <davidj@garagescript.org>
2018-02-26 09:05:04 -0800

    Solve js1_8

--- curriculum/js1/8.js
+++ curriculum/js1/8.js
@@ -5,8 +5,20 @@
  * @returns {number}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (a, b, n=1, gcd=0) => {
+  if(a%n === 0 && b%n === 0){
+    gcd = n;
+  }
+  if (a<b && a === n){
+    return gcd;
+  }
+  if (a>b && b === n){
+    return gcd;
+  }
+  if (a === b && a === n){
+    return gcd;
+  }
+  return solution(a, b, n+1, gcd);
 };
 
 module.exports = {
