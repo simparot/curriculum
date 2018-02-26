fb0e96055259917a459a311358249acd3fba17af
David J <davidj@garagescript.org>
2018-02-26 00:42:28 -0800

    Solve js1_7

--- curriculum/js1/7.js
+++ curriculum/js1/7.js
@@ -5,8 +5,17 @@
  * @returns {boolean}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (a, n=2, count=0) => {
+  if (a === 1 || count === 2){
+    return false;
+  }
+  if (a === n && count === 0){
+    return true;
+  }
+  if (a%n === 0){
+    return solution (a, n+1, count+1);
+  }
+  return solution (a, n+1, count);
 };
 
 module.exports = {
